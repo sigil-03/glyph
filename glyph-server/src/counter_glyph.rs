e6a537dee8ea48f321d928fae22f6a2f57628cec
sigil-03 <sigil@glyphs.tech>
2025-01-18 21:09:22 -0700

    add async counter glyph!

---
+++ glyph-server/src/counter_glyph.rs
@@ -0,0 +1,40 @@
+use glyph::Glyphic;
+
+pub struct Config {
+    initial_value: usize,
+}
+impl Default for Config {
+    fn default() -> Self {
+        Self {
+            initial_value: 0,
+        }
+    }
+}
+
+#[derive(Debug)]
+pub struct ExitData {
+    final_value: usize,
+}
+
+pub struct Glyph {
+    counter: usize,
+}
+
+impl Glyphic for Glyph {
+    type Config = Config;
+    type ExitData = ExitData;
+    
+    async fn load(config: Self::Config) -> Self {
+        Self {
+            counter: config.initial_value,
+        }
+    }
+
+    async fn run(mut self) -> Self::ExitData {
+        loop {
+            println!("Counter: {}", self.counter);
+            self.counter += 1;
+            tokio::time::sleep(tokio::time::Duration::from_millis(1000)).await;
+        }
+    }
+}
--- glyph-server/src/main.rs
+++ glyph-server/src/main.rs
@@ -1,3 +1,10 @@
-fn main() {
-    println!("Hello, world!");
+mod counter_glyph;
+use counter_glyph::{Glyph as CounterGlyph, Config as CounterGlyphConfig};
+use glyph::Glyphic;
+
+#[tokio::main]
+async fn main() {
+    let g1 = CounterGlyph::load(CounterGlyphConfig::default()).await;
+    let out = g1.run().await;
+    println!("output: {:#?}", out);
 }
