ca7ea72aec24f32ac0022753fbb0b6e198befc3a
sigil-03 <sigil@glyphs.tech>
2025-01-19 23:40:09 -0700

    add tcp listener

--- glyph-server/src/main.rs
+++ glyph-server/src/main.rs
@@ -1,29 +1,38 @@
-use counter_glyph::{Glyph as CounterGlyph, Config as CounterGlyphConfig};
+use clap::Parser;
+use counter_glyph::{Config as CounterGlyphConfig, Glyph as CounterGlyph};
 use glyph::Glyphic;
-use std::fs;
-
 use std::fmt::Debug;
+use std::fs;
+use tokio::net::TcpListener;
 
+#[derive(Parser)]
+struct LaunchOptions {
+    config_file: String,
+}
 
 struct GlyphServer {
     counter: CounterGlyph,
+    listener: TcpListener,
 }
 
 impl GlyphServer {
-    pub async fn load_glyph_from_file<T>(file: &str) -> T 
-    where T: Glyphic, 
-    for<'de> <T as Glyphic>::Config: serde::Deserialize<'de>,
-    <T as Glyphic>::Config: Debug,
+    pub async fn load_glyph_from_file<T>(file: &str) -> T
+    where
+        T: Glyphic,
+        for<'de> <T as Glyphic>::Config: serde::Deserialize<'de>,
+        <T as Glyphic>::Config: Debug,
     {
         let file_contents = fs::read_to_string(&file).expect("Could not read input file");
         let config: T::Config = toml::from_str(&file_contents).expect("Could not parse TOML file");
         T::load(config).await
-
     }
 
     pub async fn new() -> Self {
         Self {
             counter: Self::load_glyph_from_file("../glyphs/counter-glyph/glyph_config.toml").await,
+            listener: TcpListener::bind("127.0.0.1:8080")
+                .await
+                .expect("Failed to start TCP listener"),
         }
     }
 
@@ -33,11 +42,9 @@ impl GlyphServer {
     }
 }
 
-
 // let recipe: TomlRecipe = toml::from_str(&file).expect("Could not parse TOML file");
 
-
-//     
+//
 
 //     pub fn load(glyph: T: Glyphic) {}
 // }
